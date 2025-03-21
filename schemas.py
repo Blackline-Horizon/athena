e8012bc1bde486d2168d1377bf635e4e723f09d1
shanzi <shanzi@shanzideMacBook-Pro.local>
2025-03-21 01:30:32 -0600

    backend

--- schemas.py
+++ schemas.py
@@ -1,45 +1,45 @@
+# schemas.py
+
 from pydantic import BaseModel, Field
-from typing import List, Optional, Dict, Any, Union
-from datetime import datetime, date
+from typing import List, Optional
+from datetime import datetime
 
-class Coordinates(BaseModel):
-    latitude: float = Field(..., ge=-90.0, le=90.0)
-    longitude: float = Field(..., ge=-180.0, le=180.0)
-    radius: Optional[float] = Field(1.0, gt=0.0)  # Radius in kilometers, default to 1 km
-
-class AlertSummary(BaseModel):
-    alert_id: int
-    type_id: int
-    type_name: str
-    timesetamp: datetime
+class AlertOut(BaseModel):
+    id: int
+    device_type: Optional[str] = None
+    date_created: datetime
+    sensor_type: Optional[str] = None
+    event_type: Optional[str] = None
+    resolution_reason: Optional[str] = None
+    industry: Optional[str] = None
+    longitude: Optional[float] = None
+    latitude: Optional[float] = None
+    country: Optional[str] = None
 
     class Config:
-        from_attributes = True  # Updated from `orm_mode` to `from_attributes`
+        orm_mode = True
 
-class InsightResponse(BaseModel):
-    total_alerts: int
-    alerts: List[AlertSummary]
+class GetDashboard(BaseModel):
+    sensor_type: Optional[List[str]] = None
+    industry: Optional[List[str]] = None
+    event_type: Optional[List[str]] = None
+    resolution_reason: Optional[List[str]] = None
+    device_type: Optional[List[str]] = None
+    country: Optional[List[str]] = None
+    continent: Optional[List[str]] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
 
 class GetReport(BaseModel):
-    resolutions: List[int] = Field(...)
-    devices: List[int] = Field(...)
-    sensors: List[int] = Field(...)
-    events: List[int] = Field(...)
-    industry: Optional[List[int]] = Field(default=None)
-    date_start: date = Field(...)
-    date_end: date = Field(...)
-    countries: Optional[List[int]] = Field(default=None)
-
-class FilterParams(BaseModel):
-    sensor_types: Optional[List[str]] = None
-    industries: Optional[List[str]] = None
-    event_types: Optional[List[str]] = None
-    resolution_reasons: Optional[List[str]] = None
-    device_types: Optional[List[str]] = None
-    countries: Optional[List[str]] = None
-    continents: Optional[List[str]] = None
-    start_date: Optional[str] = None
-    end_date: Optional[str] = None
+    resolution_reason: Optional[List[str]] = None
+    device_type: Optional[List[str]] = None
+    sensor_type: Optional[List[str]] = None
+    event_type: Optional[List[str]] = None
+    industry: Optional[List[str]] = None
+    continent: Optional[List[str]] = None
+    date_start: datetime = Field(...)
+    date_end: datetime = Field(...)
+    country: Optional[List[str]] = None
 
 class ErrorResponse(BaseModel):
     detail: str
